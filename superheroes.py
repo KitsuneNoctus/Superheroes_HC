8db8d38178832eadd0b76f51b218bca6fe0b9d6b
KitsuneNoctus <theceintiant@gmail.com>
2019-09-21 13:07:02 -0700

    added functions for add armor, defend yourself, and take damage

--- superheroes.py
+++ superheroes.py
@@ -1,25 +1,18 @@
 #superheroes.py
 import random
+
 class Ability:
     def __init__(self, name, max_damage):
         self.name = name
         self.max_damage = max_damage
-        '''Create Instance Variables:
-          name:String
-          max_damage: Integer
-        '''
-        # TODO: Instantiate the variables listed in the docstring with then
-        # values passed in
         pass
 
     def attack(self):
         ''' Return a value between 0 and the value set by self.max_damage.'''
         return random.randint(0,self.max_damage)
-
         pass
 
-
-
+#=========================================================
 class Armor:
     def __init__(self, name, max_block):
         """ name takes in string, max_block is an int"""
@@ -30,7 +23,8 @@ class Armor:
     def block(self):
         return random.randint(0,self.max_block)
         pass
-
+#=========================================================
+#HERO CLASS that accesses the other objects
 class Hero:
     def __init__(self, name, starting_health=100):
         self.name = name
@@ -40,25 +34,36 @@ class Hero:
         self.current_health = starting_health
 
         pass
-
+#--------------------------------------------
     def add_ability(self, ability):
         """ Add ability to abilities list """
         self.abilities.append(ability)
         pass
 
     def attack(self):
-        total = 0
+        total_attack = 0
         for ability in self.abilities:
-            total += ability.attack()
+            total_attack += ability.attack()
 
-        return total
-        
+        return total_attack
+
+        pass
+#---------------------------------------------
+    def add_armor(self, armor):
+        self.armors.append(armor)
         pass
 
     def defend(self, incoming_damage):
-        pass
+        total_defense = 0
+        for armor in self.armors:
+            total_defense += armor.block()
 
+        return total_defense
+        pass
+#--------------------------------------------
     def take_damage(self, damage):
+        damage = damage - self.defend(damage)
+        self.current_health = self.current_health - damage
         pass
 
     def is_alive(self):
@@ -72,18 +77,23 @@ class Hero:
 if __name__=="__main__":
     #If you run this file from terminal
     #this block is executed
-
-    ability = Ability("Roasting Ability",100)
-    ability2 = Ability("DEATH",10)
-    # armor = Armor("Midget Armor",2000)
-    # print(ability.name)
-    # print(ability.attack())
-    # print(armor.name)
-    # print(armor.block())
     hero = Hero("Grace Hopper", 200)
-    hero.add_ability(ability)
-    hero.add_ability(ability2)
-    # print(my_hero.name)
-    # print(my_hero.current_health)
-    #print(hero.abilities)
-    print(hero.attack())
+    shield = Armor("Shield", 50)
+    hero.add_armor(shield)
+    hero.take_damage(50)
+    print(hero.current_health)
+
+    # ability = Ability("Roasting Ability",100)
+    # ability2 = Ability("DEATH",10)
+    #
+    # armor = Armor("Mighty Armor",2000)
+    # armor2 = Armor("Great Armor", 1000)
+    #
+    # hero.add_ability(ability)
+    # hero.add_ability(ability2)
+    #
+    # hero.add_armor(armor)
+    # hero.add_armor(armor2)
+    #
+    # print(hero.defend(10000))
+    # print(hero.attack())
