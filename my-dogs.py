a490bb1894fde3e0eb1d53d5b1ca3bad3a6a81c2
KitsuneNoctus <theceintiant@gmail.com>
2019-09-21 13:08:12 -0700

    added my-dogs.py file

---
+++ my-dogs.py
@@ -0,0 +1,14 @@
+#my-dogs.py
+from dog import Dog
+
+my_dog = Dog("Rex", "Superdog")
+my_dog.bark()
+#print(my_dog.breed)
+my_other_dog = Dog("Annie", "SuperDog")
+print(my_other_dog.name)
+
+# my_other_dog2 = Dog("Maximus", "UnderDog")
+# my_other_dog2.sit()
+# my_other_dog3 = Dog("Trigger", "SoftDog")
+# my_other_dog.roll_over()
+# my_other_dog4 = Dog("Prague", "ADog")
